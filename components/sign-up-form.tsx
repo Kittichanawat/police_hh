e33ad8b1b0c3e2f2b03399ebf5666f600e69ba59
Kittichanawat <ratchakitti2546@gmail.com>
2025-10-24 19:21:35 +0700

    add NEXT_PUBLIC_SITE_URL

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -42,7 +42,7 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
         email,
         password,
         options: {
-          emailRedirectTo: `${window.location.origin}/protected`,
+          emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/protected`,
         },
       })
       if (error) throw error
